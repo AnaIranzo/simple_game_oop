15fd85fcb72c4f00ad5ce40ca3bc51ec0f515227
AnaIranzo <aeiranzom@gmail.com>
2023-06-13 13:07:09 +0200

    add start screen

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 window.onload = function(){
     const pantallaInicio = new SplashScreen();
     pantallaInicio.crear();
+    document.addEventListener('keydown',(e)=>{
+        pantallaInicio.eliminar();
+        document.removeEventListener('keydown',null,false);
+    },false);
 }
--- splashScreen.js
+++ splashScreen.js
@@ -1,13 +1,36 @@
+const MAX_CUADRO = 2;
+const TIEMPO = 250;
+
 class SplashScreen{
     #contenedor = document.createElement('div'); //propiedad protegida solo es visible dentro de la clase
+    #idAnimacion = null
     crear(){
         this.#contenedor.classList.add('splashScreen', 'splashScreen-1');
 
         const raiz = document.getElementById('root');
         raiz.appendChild(this.#contenedor);
+
+        this.#animacion();
         
     }
     eliminar(){
-
+        this.#contenedor.remove();
+        window.clearInterval(this.#idAnimacion);
+        
     }
+
+    #animacion(){
+        let cuadro = 1;
+
+        const animacion = () => {
+            this.#contenedor.classList.remove(`splashScreen-${cuadro}`);
+            cuadro ++;
+            if (cuadro > MAX_CUADRO) {
+                cuadro = 1;
+            }
+            this.#contenedor.classList.add(`splashScreen-${cuadro}`);
+        }
+        this.#idAnimacion = setInterval(animacion, TIEMPO);
+    }
+
 }
