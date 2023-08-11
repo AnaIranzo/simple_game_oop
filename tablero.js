c9d1a3ad3e77391fea3c212791ea72a792cbf030
AnaIranzo <aeiranzom@gmail.com>
2023-08-11 10:18:24 +0200

    add objects

--- tablero.js
+++ tablero.js
@@ -4,19 +4,19 @@ const PARED = 1;
 const VACIA = 0;
 const PIEDRA = 2;
 const ARBUSTO = 3;
-const HONGO = 4;
+const SETA = 4;
 const PERSONAJE = 5;
 const nivel1 = [
     [1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,5,0,3,3,0,0,0,0,0,0,0,1],
+    [1,0,0,2,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,2,0,0,0,1],
+    [1,0,0,0,0,4,0,0,0,0,0,0,1],
+    [1,0,0,2,0,0,0,0,0,0,0,0,1],
     [1,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,2,0,0,0,1],
     [1,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,2,0,0,0,0,0,0,0,0,1],
     [1,0,0,0,0,0,0,0,0,0,0,0,1],
     [1,0,0,0,0,0,0,0,0,0,0,0,1],
     [1,1,1,1,1,1,1,1,1,1,1,1,1]
@@ -41,11 +41,19 @@ class Tablero{
                 b.setAttributeNode(a);
 
                 a = document.createAttribute('class');
-                if (nivel1[c][r]==1) {
-                    a.value = 'celda pared';
-                } else {
-                    a.value = 'celda';
-                }
+                if(nivel1[c][r]==PARED){
+					a.value = "celda pared";
+				} else if(nivel1[c][r]==PIEDRA){
+					a.value = "celda piedra";
+				} else if(nivel1[c][r]==ARBUSTO){
+					a.value = "celda arbusto";
+				} else if(nivel1[c][r]==SETA){
+					a.value = "celda seta";
+				} else if(nivel1[c][r]==PERSONAJE){
+					a.value = "celda personaje";
+				} else{
+					a.value = "celda";
+				}
                 b.setAttributeNode(a);
 
                 this.#contenedor.appendChild(b);
